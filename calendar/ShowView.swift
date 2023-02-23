7b9ed5f1163bd479b97449b3f49a1ce480c1b9f3
naotakawai <naota.kawai@gmail.com>
2023-02-24 08:46:49 +0900

    list機能

--- calendar/ShowView.swift
+++ calendar/ShowView.swift
@@ -3,33 +3,34 @@
 import SwiftUI
 
 struct ShowView: View {
-//    @State var InputView :[String] = ["温泉", "ゲーム", "美容院"]
+   
     @State var year: Int
     @State var month: Int
     @State var day: Int
-    
+    @State var schedules: [String] = []
     
     var body: some View {
         VStack {
+            
             Text("\(year), \(month), \(day)")
-            Text(UserDefaultManager.getSchedule(year: year, month: month, day: day).description)
-            
-            
-            
-            
-            
-//            List{
-//                ForEach(InputView, id: \.self) { user in
-//                    Text(user)
-//                }.onDelete { offsets in
-//                    self.InputView.remove(atOffsets: offsets)
-//                }
-//            }
-//            .padding()
-//
+            List(schedules, id: \.self) { item in
+                Text(item)
+                
+            }
+            .onDelete { indexSet in
+                schedules.remove(at: indexSet.first!)
+            }
+                
+                
+        }.onAppear{
+            schedules = UserDefaultManager.getSchedule(year: year, month: month, day: day)
         }
+            
+                     
+        }
+        
     }
-}
+
 
 struct ShowView_Previews: PreviewProvider {
     static var previews: some View {
